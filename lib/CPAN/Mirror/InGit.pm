23bbe9aa249109ff644429541723f284aa0176b3
Michael Conrad <mike@nrdvana.net>
2024-05-14 17:50:43 -0400

    Full working demo of downloading new packages and adding them to commits
    A branch holds a package list.  When a client requests a file that
    doesn't exist in the branch, it first checks the master cache (shared
    by all branches) and if it isn't there, it fetches it from CPAN. Then
    it adds the file to the master cache, and adds the file ot the current
    branch.  It delays the commit of these changes by some number of
    seconds, 10 by default.

--- lib/CPAN/Mirror/InGit.pm
+++ lib/CPAN/Mirror/InGit.pm
@@ -7,7 +7,12 @@ use v5.36;
 
 has repo => ( is => 'ro', required => 1, coerce => \&_open_repo );
 has upstream_mirror => ( is => 'ro', required => 1, default => 'https://www.cpan.org/' );
+has package_cache_branch_name => ( is => 'ro', default => 'package-cache' );
 has snapshots => ( is => 'rw' );
+
+sub signature {
+   Git::Raw::Signature->now("CPAN::Mirror::InGit", 'CPAN::Mirror::InGit@localhost');
+}
 
 sub _open_repo($thing) {
    return $thing if blessed($thing) && $thing->isa('Git::Raw::Repository');
@@ -15,67 +20,94 @@ sub _open_repo($thing) {
 }
 
 sub snapshot($self, $branch_or_tag_or_id) {
-   my $tree= $self->lookup_path($branch_or_tag_or_id);
-   return !$tree? undef
-      : $self->_snapshot_for_tree($tree, Git::Raw::Branch->lookup($self->repo, $branch_or_tag_or_id, 1));
-}
-
-sub lookup_path($self, $branch_or_tag_or_id, $path= undef) {
-   my $tree;
-   if (my $branch= Git::Raw::Branch->lookup($self->repo, $branch_or_tag_or_id, 1)) {
-      $tree= $branch->peel('tree');
-   } elsif (my $tag= eval { Git::Raw::Tag->lookup($self->repo, $branch_or_tag_or_id) }) {
-      $tree= $tag->peel('tree');
-   } elsif (my $obj= eval { $self->repo->lookup($branch_or_tag_or_id) }) {
-      $tree= $obj->type == Git::Raw::Object::COMMIT()? Git::Raw::Commit->lookup($self->repo, $branch_or_tag_or_id)->tree
-         : $obj->type == Git::Raw::Object::TREE()? Git::Raw::Tree->lookup($self->repo, $branch_or_tag_or_id)
-         : $obj->type == Git::Raw::Object::TAG()? Git::Raw::Tag->lookup($self->repo, $branch_or_tag_or_id)->target
-         : undef;
+   my ($tree, $origin)= $self->lookup_tree($branch_or_tag_or_id);
+   if (blessed($branch_or_tag_or_id) && $branch_or_tag_or_id->isa('Git::Raw::Branch') && !$origin) {
+      Carp::confess();
    }
-   return undef unless defined $tree;
-   return $tree unless defined $path && length $path;
-   my $dirent= $tree->entry_bypath($path);
-   return !$dirent? undef
-      : $dirent->type == Git::Raw::Object::TREE()? Git::Raw::Tree->lookup($self->repo, $dirent->id)
-      : $dirent->type == Git::Raw::Object::BLOB()? Git::Raw::Blob->lookup($self->repo, $dirent->id)
-      : undef;
-}
-
-sub _snapshot_for_tree($self, $tree, $branch) {
-   $self->{snapshots}{$tree->id} //=
-      CPAN::Mirror::InGit::Snapshot->new(
+   if ($origin && ref $origin eq 'Git::Raw::Branch') {
+      return $self->{snapshots}{$origin->name} //= 
+         CPAN::Mirror::InGit::Snapshot->new(
+            parent => $self,
+            branch => $origin,
+            tree => $tree,
+         );
+   } else {
+      return CPAN::Mirror::InGit::Snapshot->new(
          parent => $self,
-         branch => $branch,
          tree => $tree,
       );
+   }
 }
 
-sub commit($self, $snapshot) {
-   ...
+sub package_cache($self) {
+   my $branch= Git::Raw::Branch->lookup($self->repo, $self->package_cache_branch_name, 1);
+   if (!$branch) {
+      # Create an empty directory
+      my $empty_dir= Git::Raw::Tree::Builder->new($self->repo)->write
+         or croak;
+      my $signature= $self->signature
+         or croak;
+      # Wrap it with an initial commit
+      my $commit= Git::Raw::Commit->create($self->repo, "Initial empty tree", $signature, $signature,
+         [], $empty_dir, 'refs/heads/'.$self->package_cache_branch_name)
+         or croak;
+      # Create the branch
+      #$branch= Git::Raw::Branch->create($self->repo, $self->package_cache_branch_name, $commit)
+      #   or croak;
+   }
+   return $self->snapshot($branch);
+}
+
+sub lookup_tree($self, $branch_or_tag_or_id) {
+   my ($tree, $origin);
+   defined $branch_or_tag_or_id or croak "missing argument";
+   if (blessed($branch_or_tag_or_id) && (
+         $branch_or_tag_or_id->isa('Git::Raw::Branch')
+      || $branch_or_tag_or_id->isa('Git::Raw::Tag')
+   )) {
+      $origin= $branch_or_tag_or_id;
+      $tree= $origin->peel('tree');
+   }
+   elsif ($origin= eval { Git::Raw::Branch->lookup($self->repo, $branch_or_tag_or_id, 1) }) {
+      $tree= $origin->peel('tree');
+   } elsif ($origin= eval { Git::Raw::Tag->lookup($self->repo, $branch_or_tag_or_id) }) {
+      $tree= $origin->peel('tree');
+   } elsif (my $obj= eval { $self->repo->lookup($branch_or_tag_or_id) }) {
+      if ($obj->type == Git::Raw::Object::COMMIT()) {
+         $origin= Git::Raw::Commit->lookup($self->repo, $obj->id);
+         $tree= $origin->tree;
+      } elsif ($obj->type == Git::Raw::Object::TREE()) {
+         $tree= Git::Raw::Tree->lookup($self->repo, $obj->id);
+      } elsif ($obj->type == Git::Raw::Object::TAG()) {
+         $origin= Git::Raw::Tag->lookup($self->repo, $obj->id);
+         $tree= $origin->target;
+      }
+   }
+   return wantarray? ($tree, $origin) : $tree;
 }
 
 package CPAN::Mirror::InGit::Snapshot {
    use Carp;
    use Moo;
    use PerlIO::gzip;
+   use Scalar::Util 'refaddr', 'blessed';
    use Mojo::IOLoop;
    use v5.36;
 
-   has parent => ( is => 'ro', weak_ref => 1, required => 1 );
-   has branch => ( is => 'ro' );
-   sub upstream_mirror($self) { $self->parent->upstream_mirror }
-   sub repo($self) { $self->parent->repo }
-   has tree => ( is => 'ro', required => 1 );
-   has files => ( is => 'lazy' );
-   has packages => ( is => 'lazy' );
-   has useragent => ( is => 'lazy' );
-   has uncommitted => ( is => 'lazy', predicate => 1 );
+   has parent            => ( is => 'ro', weak_ref => 1, required => 1 );
+   has branch            => ( is => 'ro' );
+   has tree              => ( is => 'ro', required => 1 );
+   sub upstream_mirror      { shift->parent->upstream_mirror }
+   sub repo                 { shift->parent->repo }
+   has manifest_files    => ( is => 'lazy', clearer => 1, predicate => 1 );
+   has manifest_packages => ( is => 'lazy', clearer => 1, predicate => 1 );
+   has useragent         => ( is => 'lazy' );
 
-   sub _build_files($self) {
+   sub _build_manifest_files($self) {
       my $dirent= $self->tree->entry_bypath('modules/02packages.details.txt.gz')
-         or croak "modules/02packages.details.txt.gz not found";
+         or do { carp "modules/02packages.details.txt.gz not found"; return {} };
       $dirent->type == Git::Raw::Object::BLOB()
-         or croak "modules/02packages.details.txt.gz is not a file";
+         or do { croak "modules/02packages.details.txt.gz is not a file"; return {} };
       my $blob= Git::Raw::Blob->lookup($self->repo, $dirent->id);
       my %files;
       my $content= $blob->content;
@@ -94,11 +126,7 @@ package CPAN::Mirror::InGit::Snapshot {
       \%files;
    }
    
-   sub _build_uncommitted($self) {
-      return {};
-   }
-   
-   sub _build_packages($self) {
+   sub _build_manifest_packages($self) {
       my %pkg;
       my $files= $self->files;
       for my $fname (keys $files->%*) {
@@ -110,7 +138,78 @@ package CPAN::Mirror::InGit::Snapshot {
       return Mojo::UserAgent->new;
    }
    
-   sub open_file($self, $path) {
+   sub _build_uncommitted($self) {
+      return {};
+   }
+   
+   our %pending_commits;
+   END {
+      my @todo= values %pending_commits;
+      _resolve_pending_commit($_->{snapshot}, $_->{timer_id}, 'Mojo::IOLoop')
+         for @todo;
+   }
+   
+   sub delayed_commit($self, $path, $blob, $delay=10) {
+      $self->branch or croak "Can't commit without a branch";
+      warn "Will commit changed path $path to branch ".$self->branch->name;
+      my $pending_commit= $pending_commits{refaddr $self} //= {
+            cpan_ingit => $self->parent,
+            snapshot => $self,
+            changes => {},
+            packages_added => 0,
+         };
+      Mojo::IOLoop->remove($pending_commit->{timer_id})
+         if defined $pending_commit->{timer_id};
+      my @parts= split '/', $path;
+      my $basename= pop @parts;
+      my $node= $pending_commit->{changes};
+      $node= $node->{$_} //= {} for @parts;
+      $node->{$basename}= $blob;
+      ++$pending_commit->{packages_added};
+      push $pending_commit->{distfiles}->@*, $1
+         if $path =~ m|author/id/(.*)|;
+      my $id;
+      $id= $pending_commit->{timer_id}= Mojo::IOLoop->timer($delay => sub($loop) {
+         _resolve_pending_commit($self, $id, $loop);
+      });
+   }
+
+   sub _resolve_pending_commit($self, $id, $loop) {
+      my $pending_commit= $pending_commits{refaddr $self};
+      $loop->remove($id);
+      # Ignore this callback if it isn't the most recent
+      return unless $id eq $pending_commit->{timer_id};
+      # remove from global list
+      delete $pending_commits{refaddr $self};
+      my $tree= _assemble_tree($self->repo, $self->tree, $pending_commit->{changes});
+      my $sig= $self->parent->signature;
+      my $msg= join "\n",
+         "Added $pending_commit->{packages_added} dists",
+         "",
+         map "  * $_", $pending_commit->{distfiles}->@*;
+      my $parent= [ $self->branch->peel('commit') ];
+      my $commit= Git::Raw::Commit->create($self->repo, $msg, $sig, $sig, $parent, $tree)
+         or croak;
+      # Update the branch
+      $self->branch->target($commit);
+      $self->branch->target->id eq $commit->id
+         or croak "Branch object didn't update";
+      $self->tree($tree);
+   }
+   sub _assemble_tree($repo, $tree, $changes) {
+      my $treebuilder= Git::Raw::Tree::Builder->new($repo, ($tree? ($tree) : ()));
+      for my $name (keys %$changes) {
+         my $dirent= $treebuilder->get($name);
+         if (ref $changes->{$name} eq 'HASH') {
+            my $subdir= $dirent && $dirent->type == Git::Raw::Object::TREE()? Git::Raw::Tree->lookup($repo, $dirent->id) : undef;
+            $changes->{$name}= _assemble_tree($repo, $subdir, $changes->{$name});
+         }
+         $treebuilder->insert($name, $changes->{$name}, $changes->{$name}->is_tree? 0040000 : 0100644);
+      }
+      return $treebuilder->write;
+   }
+   
+   sub get_blob($self, $path) {
       my $dirent= $self->tree->entry_bypath($path);
       if ($dirent) {
          if ($dirent->type != Git::Raw::Object::BLOB()) {
@@ -118,32 +217,34 @@ package CPAN::Mirror::InGit::Snapshot {
             return undef;
          }
          warn "found blob '".$dirent->id."'";
-         my $blob= Git::Raw::Blob->lookup($self->repo, $dirent->id);
-         my $content= $blob->content;
-         return $content;
-         #open my $fh, '<:raw', \$content or die "open(BLOB): $!";
-         #return $fh;
-      } elsif ($path =~ m,^authors/id/(.*), and $self->files->{$1}) {
+         return Git::Raw::Blob->lookup($self->repo, $dirent->id);
+      } elsif ($path =~ m,^authors/id/(.*), and $self->manifest_files->{$1}) {
          warn "File '$path' should exist; not cached";
          # Check the 'cache' branch for authors/id/*
-         my $content;
-         warn "Check master cache";
-         my $obj= $self->parent->lookup_path('cache', $path);
-         if ($obj && $obj->is_blob) {
-            warn "Found in master cache";
-            $content= $obj->content;
-         } else {
+         my $package_cache= $self->parent->package_cache;
+         my $blob;
+         $blob= $package_cache->get_blob($path)
+            if $package_cache && $package_cache != $self;
+         if (!$blob) {
             # Download from upstream
             warn "Download from upstream";
-            my $tx= $self->useragent->get($self->upstream_mirror . $path);
-            $content= $tx->result->body;
+            my $tx= $self->useragent->get($self->upstream_mirror . $path)
+               or croak;
+            if (!$tx->result->is_success) {
+               warn "Failed to find file upstream: ".$tx->result->extract_start_line;
+               return undef;
+            }
+            $blob= Git::Raw::Blob->create($self->repo, $tx->result->body)
+               or croak;
+
+            # Add to the package cache branch
+            $package_cache->delayed_commit($path => $blob)
+               if $package_cache && $package_cache != $self;
          }
-         # save into repo
-         warn "Save into index";
-         $self->uncommitted->{$path}= $content;
-         # TODO: save into 'cache' repo if authors/id/
-         
-         return $content;
+         # If this is a writable branch, save this ref to commit later
+         $self->delayed_commit($path => $blob)
+            if $self->branch;
+         return $blob;
       } else {
          warn "No found, not supposed to be cached";
          return undef;
